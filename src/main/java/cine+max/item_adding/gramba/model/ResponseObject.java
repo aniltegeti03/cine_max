70014a3375b868eddbbdf16280b105312f683563
aniltegeti03 <aniltegeti@gmail.com>
2021-09-05 22:39:21 +0530

    item adding developed by anil

---
+++ src/main/java/cine+max/item_adding/gramba/model/ResponseObject.java
@@ -0,0 +1,19 @@
+package cine+max.item_adding.gramba.model;
+
+import be.cloudway.gramba.annotations.Reflected;
+
+@Reflected
+public class ResponseObject {
+    private String response;
+
+    public ResponseObject() {
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
